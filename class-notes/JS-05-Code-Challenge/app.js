985d52b9d9f664fdb5827e118287482afef1db77
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-23 15:06:15 +0200

    second commit

--- class-notes/JS-05-Code-Challenge/app.js
+++ class-notes/JS-05-Code-Challenge/app.js
@@ -12,7 +12,7 @@ console.log(false && 1); // false
 console.log(1 && 3); //3
 console.log(0 && 1); // 0
 console.log(NaN && 1); //NaN
-console.log([] && 1);
+console.log([] && 1);//Boolen degeri icine yazilinca console da görünür.
 console.log(Boolean([])); //true
 
 //OR OPERATÖRÜ
