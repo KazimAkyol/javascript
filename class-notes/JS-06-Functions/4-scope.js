2e42849e8fdd56d160e3c7a4e97be655a0476840
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-25 16:33:24 +0200

    33,commit

--- class-notes/JS-06-Functions/4-scope.js
+++ class-notes/JS-06-Functions/4-scope.js
@@ -32,3 +32,52 @@ console.log(second);
 
 //*********************** */
 
+let number = 11; //!global scope
+
+const fonksiyon = () => {
+  let number = 56; //!function-scope
+  number++;
+
+  return number;
+};
+
+console.log(number); //11
+
+console.log(fonksiyon()); //57
+
+//! zorlamayla icerideki number'i fonksiyon return'ü sayesinde, globaldeki number number=fonksiyon()//golbal scope'taki number'i function scope'taki number console.log((number))//57
+
+// const number=157 // Identifier 'number' has already been declared
+
+//* if-for gibi süslüler block-scope
+
+{
+  number++;
+
+  let sayi = 89;
+  let sayi2 = sayi + 12;
+
+  console.log(sayi2); //101
+}
+
+console.log(number); //12
+//console.log(sayi2);//süslü icinde olan süslü icinde kalir
+
+//? VAR datatype
+
+number2 = 100; //! hoisted (number2 ye sonradan type atadım ve type i her VAR  gibi oldu)
+
+var number2 = 123;
+console.log(number2);
+
+var result = "HALLO";
+
+if (number2 == 105) {
+  let result = "HI!"; //! block-scope
+
+  console.log(result);
+} else {
+  result = "SALUT"; //!global den gelen result
+}
+
+console.log(result);
