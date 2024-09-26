4bbe941df5f6df852cee5fd1e3e9f6dfd6f3aa26
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-26 10:53:17 +0200

    37.commit

---
+++ class-notes/JS-07-CodeClass_2/app.js
@@ -0,0 +1,16 @@
+//! 1- Celsius değerini Fahrenheit değerine dönüştüren bir fonksiyon yazınız? (Celsius × 9/5) + 32 = Fahrenheit
+
+//! 2- Belirli bir string ifadenin tersini bulan fonksiyonu yazınız?
+
+//! 3- Belirli bir string ifadenin palindrome olup olmadığını bulan fonksiyonu yazınız?
+
+const string1 = "selles";
+
+
+console.log(isPalindrome(string1)); //true
+
+//! 4- Belirli bir sayinin tam bölenlerinin toplamini bulan fonksiyonu yaziniz.
+
+const sumDivisors = function (num) {};
+
+//! 5-Yarıçapı verilen bir dairenin alan ve çevresini bulan program?
