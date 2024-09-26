21f8fc0911b374e94d85a825c90fd80cc03ccb71
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-26 11:17:40 +0200

    38.commit

--- class-notes/JS-07-CodeClass_2/app.js
+++ class-notes/JS-07-CodeClass_2/app.js
@@ -6,11 +6,33 @@
 
 const string1 = "selles";
 
-
-console.log(isPalindrome(string1)); //true
+//console.log(isPalindrome(string1)); //true
 
 //! 4- Belirli bir sayinin tam bölenlerinin toplamini bulan fonksiyonu yaziniz.
 
 const sumDivisors = function (num) {};
 
 //! 5-Yarıçapı verilen bir dairenin alan ve çevresini bulan program?
+
+//! 6- Belirli vir sayinin asal olup olmadigini bulan fonksiyonu yaziniz:
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+};
+
+console.log(isPrime(71));
+
+//! 7- ilk 2 parametreyi carpip 3.parametre üssüne alan fonsiyonu yaziniz:
+
+const transmogrifier = (val1, val2, val3) => {
+  let result = Math.pow(val1 * val2, val3);
+  console.log(result);
+};
+
+transmogrifier(3, 4, 2);
