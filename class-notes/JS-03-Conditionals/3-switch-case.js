16acec61d8ad46b5728dca1ae8e8afcd99e4010d
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-27 22:49:33 +0200

    69.commit

--- class-notes/JS-03-Conditionals/3-switch-case.js
+++ class-notes/JS-03-Conditionals/3-switch-case.js
@@ -1,9 +1,9 @@
 //! ****************** SWITCH - CASE ***********
 //! ---------------------------------------------
 
-// Switch -case bloğu if-else gibi kullanılacak if-else alternatif bir yöntemdir.
-// Burda bir şart durumunun karşılacağı birden fazla ihtimale göre ortaya çıkabilme ihtimali olan her duruma karşılık bir yapı verilir .
-// Switch de önemli olan şey;  bir şartla karşılaştığı zaman onu uygular ve devam eder işte o noktada çıkmasını istiyoruz ve kod blogunu kırıp çıkmazsak sadece sonuncu değeri yazar.
+// Switch-case bloğu if-else gibi kullanılacak, if-else yöntemine alternatif bir yöntemdir.
+// Burda bir şart durumunun karşılasacağı birden fazla ihtimale göre ortaya çıkabilme ihtimali olan her duruma karşılık bir yapı verilir .
+// Switch de önemli olan şey; bir şartla karşılaştığı zaman onu uygular ve devam eder işte o noktada çıkmasını istiyoruz ve kod blogunu kırıp çıkmazsak (with break) sadece sonuncu değeri yazar.
 
 // let urun = prompt("Bir ürün giriniz");
 // let fiyat;
