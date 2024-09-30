7cdfab1ab07f2cea461e80dc8309a7e7a88bba82
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-30 11:13:52 +0200

    83.commit

--- class-notes/JS-10-Array-Methods/1-loops.js
+++ class-notes/JS-10-Array-Methods/1-loops.js
@@ -6,3 +6,21 @@
 //*                     FOR LOOP
 //* ======================================================
 
+//* Örnek: dizi1 array'indeki pozotof sayilarla negatif dayilari 2 ayri array'de toplayan function yaziniz:
+
+const dizi1 = [-5, 15, -78, -45, 34, 27, 4, -9, 7];
+
+const negatifler = [];
+
+const pozitifler = [];
+
+for (let i = 0; i < dizi1.length; i++) {
+  if (dizi1[i] < 0) {
+    negatifler.push(dizi1[i]);
+  } else {
+    pozitifler.push(dizi1[i]);
+  }
+}
+
+console.log(pozitifler); //15,34,27,4,7
+console.log(negatifler); //-5,-78,-45,-9
