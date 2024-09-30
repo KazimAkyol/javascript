c0f71615671b850a922798caa53f1d1a13363be8
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-30 12:16:55 +0200

    85.commit

--- class-notes/JS-10-Array-Methods/1-loops.js
+++ class-notes/JS-10-Array-Methods/1-loops.js
@@ -46,3 +46,40 @@ console.log(arabalar.join(""));
 //! 2.yol (for of)
 
 
+
+
+
+
+
+
+
+
+
+
+
+//! for loop=> Bir dizi üzerinde iteration yapmanın orjinal yollarından biridir.	
+//? forEach=> Bir dizi üzerinde iteration yapmak için daha az kod içeren daha yeni bir yoldur.
+//! for loop=>Performansta daha hızlıdır.	
+//? forEach=> Performansta geleneksel döngüden daha yavaştır.
+//! for loop=> Break ifadesi döngüden çıkmak için kullanılabilir.	
+//? forEach=> Break ifadesi, geri arama işlevi nedeniyle kullanılamaz.
+
+
+// //***** every()
+
+//? Tum diziyi itere eder ve aldigi callback fonksiyonuna gore test gerceklestirir.Tüm elemanlar icin test basarili ise true aksi takdirde false deger döndürür.
+
+const yas = [18,23,36,56,89];
+
+console.log(yas.every((a)=> a>=18)); //true
+console.log(yas.every((a)=> a>20)); //false
+
+// //***** some()
+
+
+
+
+
+
+console.log(yas.some((eleman)=> eleman >70)); //true
+console.log(yas.some((eleman)=> eleman >100)); //false
