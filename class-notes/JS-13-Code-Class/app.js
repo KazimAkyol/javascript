3377eae6d7cbab2a15ad1c84abda7515596db357
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-07 09:45:30 +0200

    142.commit

--- class-notes/JS-13-Code-Class/app.js
+++ class-notes/JS-13-Code-Class/app.js
@@ -1,15 +1,48 @@
-//* Dizi (Array) Metodlar
-//pop():dizinin son elemanini siler
-//shift():dizinin basindaki elemani siler
-//push():dizinin sonuna eleman ekler
-//unshift():dizinin basina eleman ekler
-//reverse():diziyi tersine cevirir
-// splice():(baslangic indexi, kacinci eleman secilecekse(silinecek), eklenecek eleman(item,tem2,item3))
+//? Dizi (Array) Metodlar
+//* pop(): dizinin son elemanini siler
+//* shift(): dizinin basindaki elemani siler
+//* push(): dizinin sonuna eleman ekler
+//* unshift(): dizinin basina eleman ekler
+//* reverse(): diziyi tersine cevirir
+//* splice():(baslangic indexi, kacinci eleman secilecekse(silinecek), eklenecek eleman(item,tem2,item3))
 //* sort(): sirlama yapar(stringlerde ASCII kodlarina göre siralama yapar)
 //* sort ((a,b)=>a-b)
 //* includes (deger) iceriyor mu? - (true-false)
 //* indexOf(deger): degerin kacinci indexte oldugunun gösterir
-//* join (" "): bir dizideki elemanlari birlestirerek string'e cevirir.
-//* slice(): diziden bir parcayi almamizi saglar.
-//* concat(): iki diziyi birlestirir.
-//* every(): dizi icindeki her eleman icin bir callback  fonksiyon cagirir ve call fonksiyondaki sarti sagliyorsa true döndürür.
+//* join (" "): bir dizideki elemanlari birlestirerek string'e cevirir
+//* slice(): diziden bir parcayi almamizi saglar
+//* concat(): iki diziyi birlestirir
+//* every(): dizi icindeki her eleman icin bir callback  fonksiyon cagirir ve call fonksiyondaki sarti sagliyorsa true döndürür
+
+const numbers0 = [45, 16, 22, 14, 78];
+console.log(numbers0.every((num) => num > 13)); //true
+
+//* some(): dizi icindeki her eleman icin bir callback fonksiyon cagirir ve call fonksiyondaki en az bir eleman sarti sagliyorsa true döndürür
+
+const numbers1 = [45, 16, 22, 14, 78];
+console.log(numbers1.some((num) => num > 80)); //false
+
+//* map(): bir dizi icin bir callback fonksiyon cagirir ve her eleman icin cikan sonuclari bir diziye yazdirir
+//*
+//*
+//*
+
+//! 1.Bir dizideki elemanlarin ortalmasini bulan fonksiyon:
+
+const numbers = [34, 56, 56, 87, 90];
+
+// 1.yol
+
+let toplam = 0;
+const ortalama = () => {
+  for (let i = 0; i < numbers.length; i++) {
+    toplam += numbers[i];
+  }
+  return toplam;
+};
+
+console.log(ortalama(numbers) / numbers.length); //64.6
+
+// 2.yol
+
+console.log(numbers.reduce((toplam, eleman) => toplam + eleman) / 5);
