f3aa6aea68dd19fe13166c2e898b3b606767a0ea
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-07 09:24:07 +0200

    141.commit

---
+++ class-notes/JS-13-Code-Class/app.js
@@ -0,0 +1,15 @@
+//* Dizi (Array) Metodlar
+//pop():dizinin son elemanini siler
+//shift():dizinin basindaki elemani siler
+//push():dizinin sonuna eleman ekler
+//unshift():dizinin basina eleman ekler
+//reverse():diziyi tersine cevirir
+// splice():(baslangic indexi, kacinci eleman secilecekse(silinecek), eklenecek eleman(item,tem2,item3))
+//* sort(): sirlama yapar(stringlerde ASCII kodlarina göre siralama yapar)
+//* sort ((a,b)=>a-b)
+//* includes (deger) iceriyor mu? - (true-false)
+//* indexOf(deger): degerin kacinci indexte oldugunun gösterir
+//* join (" "): bir dizideki elemanlari birlestirerek string'e cevirir.
+//* slice(): diziden bir parcayi almamizi saglar.
+//* concat(): iki diziyi birlestirir.
+//* every(): dizi icindeki her eleman icin bir callback  fonksiyon cagirir ve call fonksiyondaki sarti sagliyorsa true döndürür.
