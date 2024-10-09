4a4864f5755c5ab2534e86012f44e77e06bc106f
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-09 09:41:24 +0200

    162.commit

--- class-notes/JS-15-Events/events.js
+++ class-notes/JS-15-Events/events.js
@@ -6,10 +6,18 @@ document.querySelector(".ara").onclick = () => {
   resim.src = "./image/img.gif";
 };
 
+//* audio calistir. play() komutu HTML'deki audi'yu calistirir
+ses1.play();
+
+//* sesin volume'ünü 0 ile 1 arasinda degerler vererek ayarlayabiliriz
+
 //! baglat butonuna tiklandiginda:
 
 document.querySelector(".baglat").onclick = () => {
   resim.src = "./image/telbagla.gif";
+
+  //*audio'nun sesini kapatmak icin pause() komutu
+  ses1.pause();
 };
 
 //! konus butonuna tiklandiginda:
@@ -17,3 +25,28 @@ document.querySelector(".baglat").onclick = () => {
 document.querySelector(".btn-outline-danger").onclick = () => {
   resim.src = "./image/telfirlat.gif";
 };
+
+//! mouse ile resmin üzerine gelince
+
+// resim.onmouseover = () => {}
+
+resim.addEventListener("mouseover", () => {
+  resim.src = "./image/aslan1.jpeg";
+});
+
+//! mouse ile resmin üzerinden ayrilinca
+
+resim.addEventListener("mouseout", () => {
+  resim.src = "./image/aslan2.jpeg";
+
+  document.querySelector(".ses2").play();
+});
+
+//! klavyeden input'a veri girisi yapilirken, checked'in tikli olmasina bagli olarak, elimizi klavyeden cektigimizde olacaklar
+
+const textInput = document.querySelector("#textbox");
+const checkInput = document.querySelector("#checkbox");
+
+textInput.onkeyup = () => {
+    
+}
