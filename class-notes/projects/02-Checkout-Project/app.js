507cc56a2a04aa61a2c104274d65a696b1e10a09
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-15 11:12:05 +0200

    192.commit

---
+++ class-notes/projects/02-Checkout-Project/app.js
@@ -0,0 +1,11 @@
+//! Functions
+
+function arttir() {
+  var sonuc = document.getElementById("sonuc");
+  sonuc.value = Number(sonuc.value) + 1;
+}
+
+function azalt() {
+  var sonuc = document.getElementById("sonuc");
+  sonuc.value = Number(sonuc.value) - 1;
+}
