cf100caa87fe13def74af523424a09dd523b5f05
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-17 12:34:14 +0200

    208.commit

---
+++ class-notes/projects/03-Wallet-App/app.js
@@ -0,0 +1,13 @@
+//! veri girdisi
+
+const tarih = document.querySelector(".tarih").innerHTML;
+const miktar = document.querySelector(".miktar").innerHTML;
+const harcamaAlani = document.querySelector(".harcama-alani").innerHTML;
+
+//! veri kaydetme
+
+document.querySelectorAll(".harcama-formu").forEach(())
+
+//! veri ekleme
+
+//! 
