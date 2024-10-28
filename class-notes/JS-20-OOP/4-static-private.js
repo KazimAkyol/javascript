fb14f9603b8a786252267e891781b5a8c6126fc7
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-28 15:19:35 +0100

    226.commit

--- class-notes/JS-20-OOP/4-static-private.js
+++ class-notes/JS-20-OOP/4-static-private.js
@@ -18,33 +18,84 @@
 //! Private metodlara ancak class icerisindeki diger metodlar ile erisilebilir.
 
 class Book {
-    //! private bir degisken tanimlayalim
+  //! private bir degisken tanimlayalim
 
-    #maas;
+  #maas;
 
-    //! static degisken tanimlamasi
+  //! static degisken tanimlamasi
 
-    static counter = 0;
+  static counter = 0;
 
-    constructor(title, yazar, year) {
-        this.title = title;
-        this.yazar = yazar;
-        this.year = year;
-        this.#maas = 50000;
-        this.ozetFunction = function () {
-            return "kitap okumak iyidir"
-        }
+  constructor(title, yazar, year) {
+    this.title = title;
+    this.yazar = yazar;
+    this.year = year;
+    this.#maas = 50000;
+    this.ozetFunction = function () {
+      return "kitap okumak iyidir";
+    };
 
-        Book.counter++;
-    }
+    Book.counter++;
+  }
 
-    //? Class icerisinde public metodlar yardimiyla private degiskenler okunabilir
-    //? Bu tip metodlara getter metod denilir.
-    //! getter metodlari class icerisinde tanimlanmalidir.
+  //? Class icerisinde public metodlar yardimiyla private degiskenler okunabilir.
+  //? Bu tip metodlara getter metod denilir.
+  //! getter metodlari class icerisinde tanimlanmalidir.
 
-    getMaas() {
-        return this.#maas;
-    }
+  getMaas() {
+    return this.#maas;
+  }
 
-    //? Class icerisisinde
+  //? Class icerisinde public metodlar yardimiyla private degiskenlere yazilabilir.
+  //? Bu tip metodlara setter metod denilir.
+  //! setter metodlar class icerisinde tanimlanmalidir.
+
+  setMaas(yenimaas) {
+    this.#maas = yenimaas;
+  }
 }
+
+const book1 = new Book("nutuk", "atatürk", 1930);
+console.log(book1);
+console.log(book1.title);
+console.log(book1.maas); // izin verilmedigi icin göremedik
+console.log(Book.counter);
+console.log(Book.maas);
+
+console.log(book1.getMaas()); // encapsulation kullanilan veri getter araciligiyla görülebilir
+
+console.log(book1.year); // 1930
+
+book1.year = 1950;
+
+console.log(book1.year); // 1950
+
+book1.setMaas(100000);
+
+console.log(book1.getMaas()); // 100000
+
+//! ABSTRACT'a Örnek
+
+function Kitap(a, b, c) {
+  //! sadece developer'in gördügü yerde tutulan veriler abstract(soyutlama) edilmis olur.
+
+  let name = a;
+  let yazar = b;
+
+  let getNoDetails = function () {
+    return `${name} kitabi $ {yazar} kisisine aittir`;
+  };
+
+  this.getDetails = function () {
+    return `book ${name} belongs to author ${yazar}`;
+  };
+
+  this.year = c;
+}
+
+let person = new Kitap("seker portakali", "jose mauro", 1800);
+
+console.log(person);
+console.log(person.name); // undefined
+console.log(person.getNoDetails); // undefined
+console.log(person.getDetails()); // book seker portakali belongs to author jose mauro
