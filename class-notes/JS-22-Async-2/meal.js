74ea7e40253f2413a1c6892738c63221dacac583
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-30 09:36:17 +0100

    235.commit

--- class-notes/JS-22-Async-2/meal.js
+++ class-notes/JS-22-Async-2/meal.js
@@ -1,6 +1,11 @@
+let yemekler = [];
+
 fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
   .then((res) => res.json())
-  .then((data) => ekranaBastir(data.meals));
+  .then((data) => {
+    yemekler = data;
+    ekranaBastir(data.meals);
+  });
 
 //! EKRANA BASTIRMA
 
@@ -9,10 +14,21 @@ const ekranaBastir = (veri) => {
 
   veri.forEach((a) => {
     foodDiv.innerHTML += `
-       <div>
+       <div class="col-lg-2 col-md-3 m-1">
        <p>${a.strMeal}</p>
        <img width="200px" src=${a.strMealThumb} />
        </div>
        `;
   });
+};
+
+//! arama inputuna yazdigim harfleri iceren yemekleri ekrana bastir (oninput=kullanici input elemanina her veri girisinde calismaktadir)
+
+//? e.target.value=> input'a her veri girisindeki degerleri alabilmek icin kullanilir. document.querySelector("input").value.target.value
+
+const input = document.querySelector("input");
+
+input.oninput = () => {
+  console.log(yemekler);
+  yemekler.meals.filter((a) => console.log());
 };
