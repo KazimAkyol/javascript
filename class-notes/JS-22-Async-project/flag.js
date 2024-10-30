e36108fcb18d58c307d4a090bd3a0236e8181264
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-30 12:22:16 +0100

    239.commit

--- class-notes/JS-22-Async-project/flag.js
+++ class-notes/JS-22-Async-project/flag.js
@@ -16,23 +16,41 @@ const ekranaBastir = (ülkeler) => {
   const devletlerDiv = document.querySelector("article");
 
   ülkeler.forEach((a) => {
+    console.log(Object.values(a.languages));
+    console.log(Object.values(a.currencies)[0].name);
+
     devletlerDiv.innerHTML += `
      <div class="card text-start w-50 shadow-lg bg-light my-4" style="width: 18rem;">
   <img src= ${a.flags.svg} class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title"> </h5>
+    <h5 class="card-title">${a.name.common}</h5>
     
 <ul class="list-group list-group-flush">
   <li class="list-group-item">
   
-  <i class="fas fa-lg fa-landmark"></i> ${a.capital} </li>
-  <li class="list-group-item">
-  <i class="fas fa-lg fa-comments"></i>
+  <i class="fas fa-lg fa-landmark"></i>
+  ${a.capital}
   </li>
-  <li class="list-group-item"> <i class="fas fa-lg fa-money-bill-wave"></i>  </li>
+  <li class="list-group-item">
+  <i class="fas fa-lg fa-comments">
+  ${Object.values(a.languages)}
+  </i>
+  </li>
+  <li class="list-group-item"> <i class="fas fa-lg fa-money-bill-wave"></i>
+  ${Object.values(a.currencies)[0].name} 
+  ${Object.values(a.currencies)[0].symbol}
+  </li>
   
 </ul>
  </div>
 </div>`;
   });
 };
+
+//!ekrana bastırılacak dizi elemanlarının key leri farklı farklıysa===>>
+//!!!! Object.values(); objectlerin value larını,   numaralandırılabilir  bir dizi döndürür, istenen nesne nin içindeki object in value larını yaz, bu value lar ,object.value sayesinde dizi içinde toplanır
+
+//? Object.values(a.currencies)=currencies in value larını bir dizide toplar
+
+//* TRY: nin value sini dizi yapar 0 indexli: [{name: 'Turkish lira', symbol: '₺'}]. TRY key i silinir
+//* Object.values(country.languages)=languages in value larını bir dizide toplar[eng,span..] Eng key i silinir
