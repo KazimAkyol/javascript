cb638da00aa63f16aa475ffeabd29a934d81ef41
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-02 21:11:29 +0100

    250.commit

--- class-notes/JS-21-Async-1/4-async-await-fetch.js
+++ class-notes/JS-21-Async-1/4-async-await-fetch.js
@@ -26,6 +26,37 @@
 const defaultImage =
   "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/220px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg";
 
+//! async-await-fetch yazimi:
+
+// const getirData = async () => {
+//   const res = await fetch("https://api.tvmaze.com/search/shows?q=girls"); // async-await komutu yazilan kodun bir süre beklemesini saglar ve süslü parantezin disina yazilan console.log("merhaba"); 'dan sonra console'a yazilir.
+
+//   const veri = await res.json();
+
+//   ekranaBastir(veri);
+
+//   // console.log("asdfghjkl");
+
+//   // console.log(res);
+// };
+
+// getirData();
+
+// // console.log("merhaba");
+
+// const ekranaBastir = (data) => {
+//   // console.log(data);
+
+//   data.forEach((a) => {
+//     document.querySelector(".users").innerHTML += `
+//     <h1> ${a.show.name} </h1>
+//     <img src = "${a.show.image?.medium || defaultImage}"/>
+//     `; // eger image cikmazsa defaultImage olarak eklenen linki yani Mona Lisa'yi kullanir.
+//   });
+// };
+
+//! async-await-fetch hata yazimi: ekrana 404 ERROR yazdirma
+
 const getirData = async () => {
   try {
     const res = await fetch("https://api.tvmaze.com/search/shows?q=girls");
@@ -44,7 +75,6 @@ const getirData = async () => {
     document.querySelector(".users").innerHTML = `
   <img src="./img/404.png"/>
   <h1>${error}</h1>
-  
   `;
   }
 };
