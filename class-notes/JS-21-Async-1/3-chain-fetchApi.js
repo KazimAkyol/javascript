55bbd097497388b3f6e2ac5189ade33d4cf47d8e
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-02 10:38:11 +0100

    248.commit

--- class-notes/JS-21-Async-1/3-chain-fetchApi.js
+++ class-notes/JS-21-Async-1/3-chain-fetchApi.js
@@ -10,7 +10,7 @@
 
 //?! Bu baglamda syntatic sugar benzetmesi yapilabilir.
 
-//!throw ile hata fırlatırsak kodumuz o satırda durur. Kodumuzu durdurmak istemezsek diğer sayfadaki kodları kullanırız.
+//! throw ile hata fırlatırsak kodumuz o satırda durur. Kodumuzu durdurmak istemezsek diğer sayfadaki kodları kullanırız.
 
 // Informational responses (100 – 199)
 // Successful responses (200 – 299)
@@ -23,6 +23,22 @@
 //   .then((veri) => console.log(veri));
 
 // console.log("merhaba");
+
+//! butona basilmadan önce verilerin gelmesi icin yazilan kod:
+
+// fetch("https://api.github.com/users")
+//   .then((res) => res.json())
+//   .then((veri) => ekranaBastir(veri));
+
+// const ekranaBastir = (veri) => {
+//   veri.forEach((person) => {
+//     document.querySelector(".users").innerHTML += `
+        
+//         <h1> ${person.login} </h1>
+//         <img width = "50%" src = ${person.avatar_url}/>
+//         <h3> ${person.node_id}</h3>`;
+//   });
+// };
 
 //! butona basildiktan sonra verilerin gelmesi icin yazilan kod:
 
