65d75477db64ac3c0ababa80d2fa08c066888f71
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-04 11:39:19 +0100

    254.commit

--- class-notes/JS-23-Weather-App/app1.js
+++ class-notes/JS-23-Weather-App/app1.js
@@ -9,6 +9,7 @@ const cardContainer = document.getElementById("card-container");
 
 const apiKey = "d9742631ab44926faeb8d55a66b17a05";
 let url; // API istegi icin kullanacagiz
+let cities = [];
 
 //! Event Listeners
 
@@ -18,12 +19,13 @@ form.addEventListener("submit", (e) => {
   if (input.value) {
     const city = input.value;
 
-    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    console.log(url);
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    // console.log(url);
     getWeatherData();
   }
 
-  form.reset();
+  form.reset(); // formu sifirlar
 });
 
 //! Functions
@@ -36,7 +38,7 @@ const getWeatherData = async () => {
   } catch (error) {
     // console.log(error); //& API ile gelen veri
 
-    //? Dta Destructure
+    //? Data Destructure
 
     const { main, name, weather, sys } = response; //& fetch
 
@@ -46,34 +48,47 @@ const getWeatherData = async () => {
 
     // console.log(iconUrl); // hava durumu koduna göre API'den icon getirmek icin
 
-    let card = `       <div class="col" id="${name}">
-    <div class="card mb-4 rounded-3 shadow-sm">
+    if (cities.indexOf(name) == -1) {
+      cities.unshift(name);
+
+      let card = `
+      <div class="col" id="${name}">
+        <div class="card mb-4 rounded-3 shadow-sm">
             <ul class="list-unstyled mt-2 mb-4">
                 <li class="text-end me-2"><i class="bi bi-x-circle"></i></li>
-                <h4 class="my-0 fw-normal">${name} <span ><sup><img src="https://flagsapi.com/${
-      sys.country
-    }/shiny/24.png" class="rounded-circle" alt=${
-      sys.country
-    }/> </sup></span></h4>
-                <h1 class="card-title pricing-card-title"><i class="bi bi-thermometer-half"></i> ${Math.round(
-                  main.temp
-                )}<sup>°C</sup></h1>
-                <h6 class="card-title pricing-card-title">Min : ${Math.round(
-                  main.temp_min
-                )}<sup>°C</sup> - Max : ${Math.round(
-      main.temp_max
-    )}<sup>°C</sup>  </h6>
-                <h6 class="card-title pricing-card-title"><img src="./assets/wi-barometer.svg" height="30px"/>${
-                  main.pressure
-                } <img src="./assets/wi-humidity.svg" height="30px"/>${
-      main.humidity
-    } </h6>
-                <li><img src="${iconUrl}"/></li>
-                <li>${weather[0].description.toUpperCase()}</li>
-            </ul>
-    </div>
-    </div>`;
+                   <h4 class="my-0 fw-normal">${name} <span ><sup><img src="https://flagsapi.com/${
+        sys.country
+      }/shiny/24.png" class="rounded-circle" alt=${
+        sys.country
+      }/> </sup></span></h4>
+                    <h1 class="card-title pricing-card-title"><i class="bi bi-thermometer-half"></i> ${Math.round(
+                      main.temp
+                    )}<sup>°C</sup></h1>
+                    <h6 class="card-title pricing-card-title">Min : ${Math.round(
+                      main.temp_min
+                    )}<sup>°C</sup> - Max : ${Math.round(
+        main.temp_max
+      )}<sup>°C</sup>  </h6>
+                    <h6 class="card-title pricing-card-title"><img src="./assets/wi-barometer.svg" height="30px"/>${
+                      main.pressure
+                    } <img src="./assets/wi-humidity.svg" height="30px"/>${
+        main.humidity
+      } </h6>
+                    <li><img src="${iconUrl}"/></li>
+                    <li>${weather[0].description.toUpperCase()}</li>
+                </ul>
+        </div>
+        </div>`;
 
-    cardContainer.innerHTML = card + cardContainer.innerHTML
+      cardContainer.innerHTML = card + cardContainer.innerHTML; // son aranani basa tutturur
+      // cardContainer.innerHTML = cardContainer.innerHTML + card; // son aranani sona tutturur
+    } else {
+      alertMessage.textContent = `You already know the weather for ${name}, Please search for another city`;
+      alertMessage.classList.replace("d-none", "d-block");
+
+      setTimeout(() => {
+        alertMessage.classList.replace("d-block", d - none);
+      }, 3000);
+    }
   }
 };
