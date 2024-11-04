a5d3b4e0f62c2db3fbf2da52a051d679d1b23eab
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-04 10:36:24 +0100

    252.commit

--- class-notes/JS-23-Weather-App/app1.js
+++ class-notes/JS-23-Weather-App/app1.js
@@ -8,9 +8,26 @@ const cardContainer = document.getElementById("card-container");
 //! Variables
 
 const apiKey = "d9742631ab44926faeb8d55a66b17a05";
+let url; // API istegi icin kullanacagiz
 
 //! Event Listeners
 
-form.addEventListener("submit", () => {
-  console.log("Form submit edildi");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const city = input.value;
+
+  url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  console.log(url);
 });
+
+//! Functions
+
+const getWeatherData = async () => {
+  try {
+    const response = await fetch(url).then((response) => response.json()); //& fetch ile API istegi
+
+    console.log(response);
+  } catch (error) {
+   // console.log(error); //& API ile gelen veri
+  }
+};
